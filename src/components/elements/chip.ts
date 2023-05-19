00a2b0860462a6cad82b46fff0c5377282dddf15
ali khosro <heydari@gmail.com>
2023-05-18 17:51:10 -0700

    animation for chips

--- src/components/elements/chip.ts
+++ src/components/elements/chip.ts
@@ -3,11 +3,11 @@ import { LitElement, css, html } from "lit";
 
 export class Chip extends LitElement {
     render() {return html`
-        <slot></slot>
+        <div><slot></slot></div>
     `}
 
     static styles = css`
-        :host {
+        div {
             width: 6em;
             height: 6em;
             padding: 1.5em;
@@ -23,7 +23,25 @@ export class Chip extends LitElement {
             box-shadow: var(--big-shadow);
             border-radius: 2em;
             cursor: pointer;
+            animation-duration: 1s;
+            transition: 2s;
         }
+
+        div:hover {
+            box-shadow: var(--outset);
+            background-color: var(--accent-background);
+
+            animation-name: bounce;
+            animation-timing-function: linear;
+            animation-iteration-count: 2;
+        }
+
+        @keyframes bounce {
+            0%   { transform: translateY(0); }
+            50%  { transform: translateY(-10px); }
+            100% { transform: translateY(0); }
+        }
+
 
         :host([disabled]) {
             pointer-events: none;
